9a70810fad41a22dba69cc5a7d5335c93f97264b
rexaulrana <rexaul.rana@gmail.com>
2023-11-30 23:52:09 +0600

    footer update

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content mt-10 mb-10">
         <aside>
           <p>
             Copyright © 2023 - All right reserved by{" "}
